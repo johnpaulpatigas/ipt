4b08a1a195ea63a8c2087ecd6ab8f5bcef7942e3
johnpaulpatigas <johnpaulpatigas@gmail.com>
2026-04-15 13:00:59 +0800

    Switch back to tailwind cdn

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,3 @@
-@vite(['resources/css/app.css', 'resources/js/app.js'])
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -8,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>IPT App</title>
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
+    <script src="https://cdn.tailwindcss.com"></script>
+
 </head>
 
 <body class="bg-gray-100 flex flex-col min-h-screen">
